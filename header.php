31da0ade9c71d1d8180ad6471e96598bd73ff0d9
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-18 13:35:09 -0800

    Changing the menu and copying basic CSS

--- header.php
+++ header.php
@@ -64,7 +64,7 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'popperscores' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 

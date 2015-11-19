0c13cd88fa8eba12e7f08bd833a7fc143b3b433a
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-19 12:05:44 -0800

    Change the order of comments and post navigation

--- single.php
+++ single.php
@@ -16,14 +16,14 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
-
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
+			
+			<?php the_post_navigation(); ?>
 
 		<?php endwhile; // End of the loop. ?>
 

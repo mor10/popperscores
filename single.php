44e5c633280ffcdd2f2e1528bc5d87d67d63b53f
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-20 15:28:51 -0800

    Fix textdomain

--- single.php
+++ single.php
@@ -25,11 +25,11 @@ get_header(); ?>
 			
 			<?php 
 			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popper' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'popper' ) . '</span> ' .
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popperscores' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'popperscores' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popper' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'popper' ) . '</span> ' .
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popperscores' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'popperscores' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
 			) );
 			?>

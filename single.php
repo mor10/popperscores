e1e3cf9207720c3cd678bdcd708f7c092866e2b1
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-20 15:05:37 -0800

    Reconfigure and restyle post navigation.

--- single.php
+++ single.php
@@ -23,7 +23,16 @@ get_header(); ?>
 				endif;
 			?>
 			
-			<?php the_post_navigation(); ?>
+			<?php 
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popper' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'popper' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popper' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'popper' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
+			?>
 
 		<?php endwhile; // End of the loop. ?>
 

fe9bd8491d3adcdbffa5e17c5e82291f66374d5f
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-20 16:40:22 -0800

    New avatar size

--- comments.php
+++ comments.php
@@ -51,6 +51,7 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
+					'avatar_size' => '96',
 				) );
 			?>
 		</ol><!-- .comment-list -->

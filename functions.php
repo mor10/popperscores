0036a63e7bdafe1cc5cc9e569cb6e988f386f5c8
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-23 12:57:14 -0800

    Add new featured image size

--- functions.php
+++ functions.php
@@ -41,6 +41,7 @@ function popperscores_setup() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
+	add_image_size( 'popperscores-featured-image', 828, 360, true );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(

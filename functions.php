ca0ba6200db13409f87983b1dd7811c9dcc3409c
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-24 10:03:36 -0800

    Add styling for Page Title

--- functions.php
+++ functions.php
@@ -42,7 +42,6 @@ function popperscores_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
 	set_post_thumbnail_size( 828, 360, true );
-	add_image_size( 'popperscores-small-thumb', 300, 150, true ); 
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(

5ac2eb14c3373007c00d9caed06ad3ad111a7770
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-24 14:14:38 -0800

    Index Continue Reading button + remove footer meta

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		
 		<?php the_title( sprintf( '<h2 class="entry-title index-excerpt"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
@@ -22,19 +23,19 @@
 
 	<div class="entry-content index-excerpt">
 		<?php
-		the_excerpt();
-//		
-//			the_content( sprintf(
-//				
-//				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'popperscores' ), array( 'span' => array( 'class' => array() ) ) ),
-//				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-//			) );
+			the_excerpt();
 		?>
-
-
 	</div><!-- .entry-content -->
+	
+	<div class="continue-reading">
+		<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+			<?php
+				printf(
+					wp_kses( __( 'Continue reading %s', 'popperscores' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				);
+			?>
+		</a>
+	</div>
 
-	<footer class="entry-footer">
-		<?php popperscores_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->

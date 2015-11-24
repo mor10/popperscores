83919ffefb2706dcdd02076400239be418aa3241
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-24 12:22:49 -0800

    Markup and style to change display of index posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,16 +11,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title( sprintf( '<h2 class="entry-title index-excerpt"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php popperscores_posted_on(); ?>
+		<div class="index-entry-meta">
+			<?php popperscores_index_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content index-excerpt">
 		<?php
 		the_excerpt();
 //		

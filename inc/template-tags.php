cdb556ff41e98b256a0119ebb21199cb8036bdb8
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-24 11:56:04 -0800

    Add poppescores_index_posted_on()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -53,6 +53,49 @@ function popperscores_posted_on() {
 }
 endif;
 
+if ( ! function_exists( 'popper_index_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for post-date/time and author on index pages.
+ */
+function popperscores_index_posted_on() {
+	
+	$author_id = get_the_author_meta( 'ID' );
+	
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		esc_html( get_the_modified_date() )
+	);
+
+	$posted_on = sprintf(
+		esc_html_x( 'Published %s', 'post date', 'popper' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+	$byline = sprintf(
+		esc_html_x( 'by %s', 'post author', 'popper' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+	
+	echo '<div class="meta-content">';
+	echo '<span class="byline">' . $byline . ' </span><span class="posted-on">' . $posted_on . ' </span>'; // WPCS: XSS OK.
+	if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( esc_html__( 'Leave a comment', 'popper' ), esc_html__( '1 Comment', 'popper' ), esc_html__( '% Comments', 'popper' ) );
+		echo '</span>';
+	}
+	echo '</div><!-- .meta-content -->';
+
+}
+endif;
+
+
 if ( ! function_exists( 'popperscores_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.

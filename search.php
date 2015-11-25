df32b5028edf3db594baac57af333f72a6494832
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-11-25 11:28:36 -0800

    Simplify search results page by using content.php

--- search.php
+++ search.php
@@ -27,12 +27,13 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content', get_post_format() );
+
 				?>
 
 			<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
+			<?php popperscores_paging_nav(); ?>
 
 		<?php else : ?>
 
